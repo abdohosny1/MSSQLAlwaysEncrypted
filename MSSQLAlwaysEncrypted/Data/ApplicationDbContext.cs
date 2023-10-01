aa377355a5980e5cab7120a16b81e852a914e967
abdohosny1 <make.y90@yahoo.com>
2023-10-01 21:07:01 +0300

    Add project files.

---
+++ MSSQLAlwaysEncrypted/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MSSQLAlwaysEncrypted.Models;
+using System.Collections.Generic;
+
+namespace MSSQLAlwaysEncrypted.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>()
+                .HasData(new Employee[]
+                {
+                new Employee { Id = 1, SSN = "795-73-9838", FirstName = "Catherine", LastName = "Abel", Salary = 31692 },
+                new Employee { Id = 2, SSN = "990-00-6818", FirstName = "Kim", LastName = "Abercrombie", Salary = 55415 }
+                });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ MSSQLAlwaysEncrypted/Migrations/20231001174940_init.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MSSQLAlwaysEncrypted.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SSN = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Salary = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "FirstName", "LastName", "SSN", "Salary" },
+                values: new object[] { 1, "Catherine", "Abel", "795-73-9838", 31692.0 });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "FirstName", "LastName", "SSN", "Salary" },
+                values: new object[] { 2, "Kim", "Abercrombie", "990-00-6818", 55415.0 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
